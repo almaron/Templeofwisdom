cadd00b541f01b650b5489c0b4394b5add360be8
Ilya Levin <almaron@gmail.com>
2014-06-09 00:38:39 +0400

    Forum model tests fixed and working

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -5,6 +5,7 @@ class ForumPost < ActiveRecord::Base
   belongs_to :user
 
   after_create :touch_topic
+  before_destroy :check_if_last
   after_destroy :remove_post
 
   def touch_topic
@@ -13,6 +14,10 @@ class ForumPost < ActiveRecord::Base
 
   def remove_post
     self.topic.remove_post(self) if self.topic
+  end
+
+  def check_if_last
+    self.topic.last_post_id == self.id
   end
 
 end
