62a8703375cc55dd6261c6c5dadbbde76987ca39
Ilya Levin <i.levin@rambler-co.ru>
2015-04-22 18:13:31 +0300

    Mailer config

--- config/initializers/mailer_config.rb
+++ config/initializers/mailer_config.rb
@@ -1,10 +1,12 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'templeofwisdom.ru',
-  user_name:            'noreply',
-  password:             'yw6V1mjUegXQXqarm42',
-  authentication:       'plain',
-  enable_starttls_auto: true
-}
+Rails.application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'templeofwisdom.ru',
+      user_name:            'noreply',
+      password:             'yw6V1mjUegXQXqarm42',
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+end
