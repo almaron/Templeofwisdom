901e0765d61c3ec8c7d9561df0e3f223517785f9
Ilya Levin <almaron@gmail.com>
2015-05-11 11:25:18 +0300

    Proverb ssi done

---
+++ app/controllers/proverb_controller.rb
@@ -0,0 +1,19 @@
+class ProverbController < ApplicationController
+
+  layout false
+
+  def show
+    render text: @proverb = random_proverb
+  end
+
+  private
+
+  def random_proverb
+    proverb_array.sample
+  end
+
+  def proverb_array
+    File.readlines(Rails.root.join('public','pros.txt')).map {|line| line.gsub("\r\n","")}
+  end
+
+end
