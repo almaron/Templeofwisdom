702126cfbc8d7f19a8747f40a99c40d7fa2d73b9
Ilya Levin <almaron@gmail.com>
2015-05-11 10:24:09 +0300

    Show specs

--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -7,7 +7,7 @@ describe ForumsController, :type => :controller do
       create_list :forum, 4
     end
 
-    describe 'AS html' do
+    context 'AS html' do
       before do
         get :index, format: :html
       end
@@ -25,7 +25,7 @@ describe ForumsController, :type => :controller do
       end
     end
 
-    describe 'AS json' do
+    context 'AS json' do
       before do
         get :index, format: :json
       end
@@ -38,6 +38,37 @@ describe ForumsController, :type => :controller do
         expect(assigns(:forums).size).to eql 4
       end
     end
+  end
+
+  describe 'GET #show' do
+    before :each do
+      create :forum, id: 1
+      create_list :child_forum, 4
+    end
+
+    context 'AS html' do
+      before do
+        get :show, id: 1, format: :html
+      end
+
+      it 'should return ok' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'should render the view' do
+        expect(response).to render_template('show')
+      end
+
+      it 'should fetch the forum' do
+        expect(assigns :forum).to be_a Forum
+        expect(assigns(:forum).id).to eql 1
+      end
+
+      it 'should fetch children' do
+        expect(assigns(:forum).children.count).to eql 4
+      end
+    end
+
   end
 
 end
--- spec/factories/forums.rb
+++ spec/factories/forums.rb
@@ -4,9 +4,14 @@ FactoryGirl.define do
   factory :forum do
     name "MyForum"
     ancestry nil
+    is_category 1
 
     factory :child_forum do
       ancestry "1"
+    end
+
+    trait :not_category do
+      is_category 0
     end
 
   end
