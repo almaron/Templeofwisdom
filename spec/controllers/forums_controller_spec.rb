9e41335e881368d355e0a181bfd76d17534e95be
Ilya Levin <almaron@gmail.com>
2015-05-11 10:09:05 +0300

    ForumsController specs

--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -2,4 +2,42 @@ require 'rails_helper'
 
 describe ForumsController, :type => :controller do
 
+  describe 'GET #index' do
+    before :each do
+      create_list :forum, 4
+    end
+
+    describe 'AS html' do
+      before do
+        get :index, format: :html
+      end
+
+      it 'should return ok' do
+        expect(response.status).to eql(200)
+      end
+
+      it 'should render the view' do
+        expect(response).to render_template('index')
+      end
+
+      it 'should not assign forums' do
+        expect(assigns(:forums)).to be_nil
+      end
+    end
+
+    describe 'AS json' do
+      before do
+        get :index, format: :json
+      end
+
+      it 'should return ok' do
+        expect(response.status).to eql(200)
+      end
+
+      it 'should assign forums' do
+        expect(assigns(:forums).size).to eql 4
+      end
+    end
+  end
+
 end
