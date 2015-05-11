873407fe710826b0d05a169d8dd0be7911de8eab
Ilya Levin <almaron@gmail.com>
2015-05-11 16:37:40 +0300

    Specs added

--- spec/controllers/weather_controller_spec.rb
+++ spec/controllers/weather_controller_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe WeatherController, type: :controller do
   describe 'GET #show' do
-    it 'should return ok' do
+
+    before do
+      create :season
       get :show
+    end
+
+    it 'should return ok' do
       expect(response.status).to eql 200
+    end
+
+    it 'should assign a season' do
+      expect(assigns :season).to be_a Season
     end
   end
 end
---
+++ spec/factories/seasons.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :season do
+    id 1
+    slug 'winter'
+    name 'Winter'
+    description "Couple strings here\r\nAnd there"
+  end
+
+end
