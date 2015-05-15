0c0e823b6137cd3aa3de30d871c998991eee4cb1
Ilya Levin <almaron@gmail.com>
2015-05-15 23:23:59 +0300

    Revert "Ruby and Rails updated"
    This reverts commit 735b7d8167fad8edaf539a64880907cb24db7aa9.

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,5 @@ module Linyansho411
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**','*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
-
-    config.active_record.raise_in_transactional_callbacks = true
-
   end
 end
