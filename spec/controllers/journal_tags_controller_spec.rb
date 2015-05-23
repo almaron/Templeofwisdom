b7918804d64ff90eb7ff44e9a15af979c91d8a4a
Ilya Levin <almaron@gmail.com>
2015-05-23 19:49:55 +0300

    Spec fix

--- spec/controllers/journal_tags_controller_spec.rb
+++ spec/controllers/journal_tags_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe JournalTagsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      get :index, format: :json
+      get :index, format: :json, q: 'норо'
       expect(response).to have_http_status(:success)
     end
   end
