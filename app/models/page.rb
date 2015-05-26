fb8283f7bd7f760db40759fd8a8897802fea320f
Ilya Levin <i.levin@rambler-co.ru>
2015-05-26 16:50:07 +0300

    Rendering done

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,24 @@
 class Page < ActiveRecord::Base
   has_ancestry
 
+  PARTIALS = [
+    {
+      tag: 'list_subs',
+      partial: 'list_subs'
+    }
+  ]
+  def listed_children
+    self.children.where(hide_menu: 0, published: 1).order(:sort_order)
+  end
+
   def deplete!
     self.children.each {|page| page.update(parent_id: parent_id)}
     destroy
+  end
+
+  def render_content
+    text = self.content
+    PARTIALS.each { |part| text.gsub! "[[#{part[:tag]}]]", "<%= render partial: '#{part[:partial]}', locals: {page: @page} %>" }
+    text
   end
 end
