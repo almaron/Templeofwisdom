b20d77b65e0b4723796df96eb42e8298a194ca2d
Ilya Levin <i.levin@rambler-co.ru>
2015-06-02 15:43:20 +0300

    Code style

--- app/models/char_skill.rb
+++ app/models/char_skill.rb
@@ -3,7 +3,7 @@ class CharSkill < ActiveRecord::Base
   belongs_to :skill
   belongs_to :level
 
-  default_scope {order(:level_id)}
+  default_scope { order(:level_id) }
   scope :phisic, ->{includes(:skill).where(skills:{skill_type:"phisic"})}
   scope :magic, ->{includes(:skill).where(skills:{skill_type:"magic"})}
 end
