a3e70dad9843f6265d1779f56a6a434fd6b90e0c
Ilya Levin <i.levin@rambler-co.ru>
2015-06-02 10:08:20 +0300

    Class << self

--- app/services/notes/skill_request.rb
+++ app/services/notes/skill_request.rb
@@ -1,11 +1,13 @@
 module Notes
   class SkillRequest < SystemNote
-    def create(request, message="accept")
-      Notification.create({
-        user_id: request.user_id,
-        head: I18n.t("notifications.system.head.notify_skill_request.#{message}"),
-        text: render(partial: "shared/notifications/notify_skill_request_#{message}", locals: {request: request})
-      })
+    class << self
+      def create(request, message="accept")
+        Notification.create({
+          user_id: request.user_id,
+          head: I18n.t("notifications.system.head.notify_skill_request.#{message}"),
+          text: render(partial: "shared/notifications/notify_skill_request_#{message}", locals: {request: request})
+        })
+      end
     end
   end
 end
