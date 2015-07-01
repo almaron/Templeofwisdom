0e2930f0c66ad121addaed0c745d2f4e5f95cb24
Ilya Levin <almaron@gmail.com>
2015-07-01 22:33:53 +0300

    notification fix

--- app/services/notes/question.rb
+++ app/services/notes/question.rb
@@ -4,7 +4,7 @@ module Notes
       Notification.create({
         user_id: question.user_id,
         head: I18n.t('notifications.system.head.notify_user_question_answer'),
-        text: render(partial: 'shared/notifications/notify_char_accept', locals: {question: question, user: user})
+        text: render(partial: 'shared/notifications/notify_user_question_answer', locals: {question: question, user: user})
       })
     end
   end
