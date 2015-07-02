eba55077ec5989861d8f2bac094ec1cecd50a435
Ilya Levin <almaron@gmail.com>
2015-07-02 18:28:53 +0300

    removed unneeded specs

--- spec/controllers/logs_controller_spec.rb
+++ spec/controllers/logs_controller_spec.rb
@@ -1,17 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe LogsController, type: :controller do
-  describe 'GET #index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
 
-  describe 'GET #show' do
-    it 'returns http success' do
-      get :show, category: 1
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
