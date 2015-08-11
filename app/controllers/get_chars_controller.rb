3df01a970e50836b2ed922900659324cd73206be
Ilya Levin <almaron@gmail.com>
2015-08-11 21:00:08 +0300

    to_json added

--- app/controllers/get_chars_controller.rb
+++ app/controllers/get_chars_controller.rb
@@ -2,7 +2,7 @@ class GetCharsController < ApplicationController
 
   def messages
     @chars = Char.where(status_id: (2..5).to_a)
-    render json: @chars, include: [:id, :name]
+    render json: @chars.to_json(only: [:id, :name])
   end
 
 end
