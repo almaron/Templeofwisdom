d9bf03447b0cbb0e5246b4ead458a050a2adb4ee
Ilya Levin <almaron@gmail.com>
2015-08-17 10:05:44 +0300

    forum show fix

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -17,9 +17,9 @@ class ForumsController < ApplicationController
   end
 
   def show
+    @forum = Forum.find(params[:id])
     respond_to do |format|
       format.html {
-        @forum = Forum.find(params[:id])
         if current_user
           render template: 'application/error_404', status: :not_found unless @forum.is_visible?(current_user)
         else
