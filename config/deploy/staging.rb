8f37eaeaa392cf87188eeb7ed2672f4e2b99ad01
Ilya Levin <almaron@gmail.com>
2015-10-11 13:08:46 +0300

    get back ooc coloring

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,28 +7,3 @@ set :nginx_server_name, 's.templeofwisdom.ru'
 set :deploy_to, '/home/malk/rails/temple_stage'
 
 set :linked_files, %w{ config/database.yml config/secrets.yml  config/settings.yml public/robots.txt config/journals.yml}
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
