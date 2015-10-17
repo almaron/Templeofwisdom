6846b3b1ef48d8d4d25dfd82cc0bb862f11d1ccc
Ilya Levin <almaron@gmail.com>
2015-10-17 18:16:41 +0300

    topics linked

--- app/services/topic_link_parser.rb
+++ app/services/topic_link_parser.rb
@@ -1,26 +1,36 @@
 class TopicLinkParser
-  attr_reader :post, :url
+  attr_reader :post, :url, :link
 
   def initialize(post)
     @post = post
   end
 
   def parse
-    # post.update text: parsed_text
-    parsed_text
+    begin
+      post.update text: parsed_text
+    rescue StandardError => e
+
+    end
   end
 
-  private
-
-  TOPIC_REGEXP = /(\/?viewtopic.*?)[\]\[]/
+  TOPIC_REGEXP = /(\[url=?(.*?\/temple\/viewtopic.php\?.*?)?\])(.*?)\[\/url\]/mi
 
   def parsed_text
     text = post.text
-    post.text.scan(TOPIC_REGEXP).flatten.each do |url|
-      @url = url
-      text = text.gsub(url, new_path)
+    post.text.scan(TOPIC_REGEXP).each do |flap|
+      @url = flap[1] || flap[2]
+      @link = flap[2]
+      text = text.gsub(flap[0], new_url).gsub(flap[2], new_link)
     end
     text
+  end
+
+  def new_url
+    "[url=http://templeofwisdom.ru#{new_path}]"
+  end
+
+  def new_link
+    link.include?('viewtopic') ? 'ссылка' : link
   end
 
   def new_path
@@ -30,6 +40,7 @@ class TopicLinkParser
   def green_params
     hash = uri_params
     uri_params.each_pair {|k,v| hash[k] = v[0]}
+    hash
   end
 
   def uri_params
