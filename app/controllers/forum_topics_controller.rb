0331f7423e73164a04d2ddb0c9677c6e4faa1648
Ilya Levin <almaron@gmail.com>
2015-10-28 14:26:36 +0300

    fixed topic list

--- app/controllers/forum_topics_controller.rb
+++ app/controllers/forum_topics_controller.rb
@@ -9,7 +9,7 @@ class ForumTopicsController < ApplicationController
   def index
     @forum = Forum.find(params[:forum_id])
     #TODO change the clause for displaying hidden topics
-    @all_topics = (current_user && current_user.is_in?(:admin)) ? @forum.topics : @forum.topics.shown
+    @all_topics = (current_user && current_user.can_view_hidden?) ? @forum.topics : @forum.topics.shown
     per_page = params[:per_page] || 25
     @topics = @all_topics.paginate(page: params[:page], per_page: per_page) if @all_topics
     respond_to do |format|
