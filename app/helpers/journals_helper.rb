3a9b33707702575eb9b1a27cda70b5f0813a6345
Ilya Levin <almaron@gmail.com>
2015-10-30 23:00:33 +0300

    OG tags

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -7,4 +7,13 @@ module JournalsHelper
   def image_hash
     @page.images.map(&:image_url).to_json.gsub('"',"'")
   end
+
+  def og_image
+    return nil unless @page && @page.images.any?
+    @page.images.first.image_url
+  end
+
+  def og_author
+    @page && @page.is_article? ? @page.content_line : 'редакция "Монастырские Хроники"'
+  end
 end
