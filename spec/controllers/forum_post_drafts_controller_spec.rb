402ae10229e73f9263339d4a8201bcff6560ef16
Ilya Levin <almaron@gmail.com>
2017-02-05 08:14:34 +0300

    removed redirects

--- spec/controllers/forum_post_drafts_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ForumPostDraftsController, type: :controller do
-
-end
--- spec/controllers/topic_redirects_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe TopicRedirectsController, type: :controller do
-
-end
